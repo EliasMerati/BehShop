16a1001c2336f8a0e2681de0f754b5703c70562a
elias.merati <elias.merati@gmail.com>
2023-02-25 16:52:21 +0330

    CreateLogin

--- BehShop.Web/Controllers/AccountController.cs
+++ BehShop.Web/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using BehShop.Domain.Entities.User;
 using BehShop.Web.Models.ViewModels.Register;
+using BehShop.Web.Models.ViewModels.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,11 @@ namespace BehShop.Web.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
-        public AccountController(UserManager<User> userManager)
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -47,10 +50,55 @@ namespace BehShop.Web.Controllers
         }
         #endregion
 
+        #region Login
+        public IActionResult Login(string Returnurl ="/")
+        {
+            return View(new LoginViewModel
+            {
+                ReturnURL= Returnurl
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+             var user =_userManager.FindByNameAsync(login.Email).Result;
+            if (user is null)
+            {
+                ModelState.AddModelError("", "کاربری با مشخصات فوق یافت نشد");
+                return View(login);
+            }
+            _signInManager.SignOutAsync();
+            var result = _signInManager.PasswordSignInAsync(user, login.Password, login.IsPersistent, true).Result;
+            if (result.Succeeded)
+            {
+                return Redirect(login.ReturnURL);
+            }
+
+            return View(login);
+        }
+        #endregion
+
+        #region SignOut
+        public IActionResult SignOut()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+
+        #region Profile
         public IActionResult Profile()
         {
+
             return View();
         }
+        #endregion
+
 
     }
 }
---
+++ BehShop.Web/Models/ViewModels/User/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BehShop.Web.Models.ViewModels.User
+{
+#nullable disable
+    public class LoginViewModel
+    {
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress]
+        [MaxLength(100, ErrorMessage = "{0} نباید بیشتر از {1} باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool IsPersistent { get; set; }
+
+        public string ReturnURL { get; set; }
+    }
+}
