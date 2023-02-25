343e884a595437c1d5e710b3abf925206ae4940c
elias.merati <elias.merati@gmail.com>
2023-02-25 13:56:06 +0330

    UpdateIdentityDbContext

--- BehShop.Persistance/Contexts/IdentityDataBaseContext.cs
+++ BehShop.Persistance/Contexts/IdentityDataBaseContext.cs
@@ -14,6 +14,7 @@ namespace BehShop.Persistance.Contexts
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            #region Rename Identity Tables &Schema
             builder.Entity<IdentityUser<string>>().ToTable("Users", "Identity");
             builder.Entity<IdentityRole<string>>().ToTable("Roles", "Identity");
             builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "Identity");
@@ -21,13 +22,18 @@ namespace BehShop.Persistance.Contexts
             builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "Identity");
             builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Identity");
             builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "Identity");
+            #endregion
 
+            // Because OnModelCreating Is Override
+            #region Add PrimaryKey For 3 Table Of Identity
             builder.Entity<IdentityUserLogin<string>>()
-                .HasKey(p => new { p.LoginProvider, p.ProviderKey });
+        .HasKey(p => new { p.LoginProvider, p.ProviderKey });
             builder.Entity<IdentityUserRole<string>>()
                 .HasKey(p => new { p.RoleId, p.UserId });
             builder.Entity<IdentityUserToken<string>>()
                 .HasKey(p => new { p.LoginProvider, p.Name, p.UserId });
+            #endregion
+
             //base.OnModelCreating(builder);
         }
     }
