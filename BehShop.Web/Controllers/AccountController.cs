a2238120f8a363c1bc82cbf9cef5068f667dc27f
elias.merati <elias.merati@gmail.com>
2023-02-25 16:30:06 +0330

    Add Register

--- BehShop.Web/Controllers/AccountController.cs
+++ BehShop.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using BehShop.Domain.Entities.User;
+using BehShop.Web.Models.ViewModels.Register;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,12 +23,34 @@ namespace BehShop.Web.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult Register()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid) { return View(model); }
+            var user = new User()
+            {
+                FullName= model.FullName,
+                Email= model.Email,
+                UserName=model.Email,
+                PhoneNumber=model.PhoneNumber,
+            };
+           var result = _userManager.CreateAsync(user, model.Password).Result;
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Profile));
+            }
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError(item.Code, item.Description);
+            }
+            return View(model);
+        }
         #endregion
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
 
     }
 }
---
+++ BehShop.Web/Models/ViewModels/User/RegisterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BehShop.Web.Models.ViewModels.Register
+{
+#nullable disable
+    public class RegisterViewModel
+    {
+
+        [Display(Name = "نام و نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "{0} نباید بیشتر از {1} باشد")]
+        public string FullName { get; set; }
+
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress]
+        [MaxLength(100,ErrorMessage ="{0} نباید بیشتر از {1} باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "شماره تلفن")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "{0} نباید بیشتر از {1} باشد")]
+        public string PhoneNumber { get; set; }
+
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage ="کلمه های عبور مغایرت دارند")]
+        public string RePassword { get; set; }
+
+
+    }
+}
